f8e26d650323e4c371ee09d8960ae1a7a4fcadff
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-05-04 13:09:44 +0300

    added  FeedPost component, Babel presets for path aliaces

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FeedPost } from "@Components/FeedPost";
+import posts from "@Assets/data/posts.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <FlatList
+          data={posts}
+          renderItem={({ item }) => <FeedPost post={item} />}
+        />
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@Navigation": "./src/navigation",
+            "@Components": "./src/components",
+            "@Screens": "./src/screens",
+            "@Assets": "./assets"
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        }
+      ]
+    ]
   };
 };
